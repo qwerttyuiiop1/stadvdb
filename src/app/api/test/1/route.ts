ba42756150b50f1f1cd384665bf4c487749e55ef
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-04-06 18:09:19 +0800

    fix test 1

--- src/app/api/test/1/route.ts
+++ src/app/api/test/1/route.ts
@@ -11,9 +11,10 @@ export const GET = async (req: NextRequest) => {
 		SELECT * FROM foo;
 		COMMIT;
 	`.split("\n").map(s => s.trim()).filter(s => s.length > 0);
-	const e1 = await execute(query, read(process.env.NODE_1_IP)).start();
-	console.log('Test 1 executed');
-	return NextResponse.json({test: e1});
+	const ipis = [process.env.NODE_1_IP, process.env.NODE_2_IP, process.env.NODE_3_IP];
+	const queries = ipis.map(ip => execute(query, read(ip)));
+	const res = await Promise.all(queries.map(q=>q.start()));
+	return NextResponse.json(res.map(r => r[1]));
   } catch (e) {
 	console.error(e);
 	return NextResponse.json(e, { status: 500 });
