4dae5e18c45f390e05608c85c6e1c368500f4bef
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-04-08 11:30:51 +0800

    update lock/unlock

--- src/app/api/lock/route.ts
+++ src/app/api/lock/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { admin } from "../connectionController"
 
-export const GET = async () => {
+export const GET = async (req: NextRequest) => {
   await admin(async conn => {
 	conn.query("FLUSH TABLES WITH READ LOCK;");
   });
