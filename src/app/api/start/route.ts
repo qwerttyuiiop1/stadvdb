1d5163db263e81a60a9ca11d29be94e912f75b7d
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-04-08 10:49:51 +0800

    update start server

--- src/app/api/start/route.ts
+++ src/app/api/start/route.ts
@@ -1,12 +1,6 @@
 import { NextResponse } from "next/server";
 import { promisify } from "util";
-import { admin } from "../connectionController";
 const exec = promisify(require("child_process").exec);
-
-// sudo systemctl start mysql
-// find and set master ip
-// set global read_only = 1;
-// start slave
 
 export const GET = async () => {
   let { stdout, stderr } = await exec("sudo systemctl start mysql");
@@ -15,8 +9,5 @@ export const GET = async () => {
 	return NextResponse.json({ success: false });
   }
   console.log(stdout);
-  await admin(async conn => {
-	const [res] = await conn.sql("SHOW MASTER STATUS;");
-  })
   return NextResponse.json({ success: true });
 }
