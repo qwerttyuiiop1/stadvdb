d8bbed42df579f7c54d203a466a99e58192f1437
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-04-09 15:31:54 +0800

    test locks

--- src/app/api/lock/[seconds]/route.ts
+++ src/app/api/lock/[seconds]/route.ts
@@ -2,19 +2,16 @@ import { admin } from "@connect"
 import { NextResponse } from "next/server";
 type Params = {
 	params: {
-		sleep: string
+		seconds: string
 	}
 }
-export const GET = async (a: any,{params: {sleep}}: Params) => {
+export const GET = async (a: any,{params: {seconds}}: Params) => {
   try {
 	await admin(async conn => {
-	  console.log("locking");
 	  await conn.query("FLUSH TABLES WITH READ LOCK;");
-	  await new Promise(resolve => setTimeout(resolve, Number(sleep) * 1000));
-	  console.log("unlocking");
+	  await new Promise(resolve => setTimeout(resolve, Number(seconds) * 1000));
 	  await conn.query("UNLOCK TABLES;");
 	});
-	console.log("unlocked");
 	return NextResponse.json({ success: true });
   } catch (e) {
 	console.error(e);
