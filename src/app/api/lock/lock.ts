c51be72d291e6426fd96057852d35d2b392394ba
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-04-09 15:12:26 +0800

    test locks

--- src/app/api/lock/lock.ts
+++ src/app/api/lock/lock.ts
@@ -1,25 +1,28 @@
 import { ScopedConnection, admin, scope } from "../connectionController"
 const adminScope = scope(admin);
 
-const any = global as any;
-any.lock = {
-    conn: null,
-    lock: async function() {
-        if (this.conn) return;
-        this.conn = await adminScope(undefined, 'self');
-        await this.conn.query("FLUSH TABLES WITH READ LOCK;");
-        console.log(Boolean(this.conn));
-    },
-    unlock: async function() {
-        console.log(Boolean(this.conn));
-        if (!this.conn) return;
-        console.log("unlocking");
-        await this.conn.query("UNLOCK TABLES;");
-        await this.conn.endScope();
-        console.log("unlocked");
-        this.conn = null;
-    }
+const global = globalThis as any;
+if (global.lock === undefined) {
+	global.lock = function() {
+		let conn: ScopedConnection | null = null;
+		const lock = async () => {
+			if (conn) return;
+			conn = await adminScope(undefined, 'self');
+			await conn.query("FLUSH TABLES WITH READ LOCK;");
+			console.log(Boolean(conn));
+		}
+		const unlock = async () => {
+			console.log(Boolean(conn));
+			if (!conn) return;
+			console.log("unlocking");
+			await conn.query("UNLOCK TABLES;");
+			await conn.endScope();
+			console.log("unlocked");
+			conn = null;
+		}
+		return {lock, unlock};
+	}
 }
 
-export const lock = any.lock.lock as () => Promise<void>;
-export const unlock = any.lock.unlock as () => Promise<void>;
+export const lock = global.lock.lock as () => Promise<void>;
+export const unlock = global.lock.unlock as () => Promise<void>;
