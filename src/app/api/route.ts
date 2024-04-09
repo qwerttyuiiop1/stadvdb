4c90e53a556a0ab04311ca77ed9b46cc8e7214b9
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-04-10 00:45:54 +0800

    disable remote sql execution :(

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,13 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import mysql from "mysql2/promise";
 export const POST = async (req: NextRequest) => {
   try {
-	const { query, server } = await req.json();
-	const conn = await mysql.createConnection({
-	  host: server, user: process.env.ADMIN_USER, password: process.env.ADMIN_PASSWORD
-	});
-	const [res] = await conn.query(query);
-	conn.end();
-	return NextResponse.json({ res });
+	// const { query, server } = await req.json();
+	// const conn = await mysql.createConnection({
+	//   host: server, user: process.env.ADMIN_USER, password: process.env.ADMIN_PASSWORD
+	// });
+	// const [res] = await conn.query(query);
+	// conn.end();
+	// return NextResponse.json({ res });
+	return NextResponse.json({error: "This function has been disabled."});
   } catch (e) {
 	console.error(e);
 	return NextResponse.json({error: e});
