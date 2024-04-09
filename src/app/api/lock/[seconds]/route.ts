824d3210c5e3d5652563a7baf46cba5efdc9e779
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-04-09 15:31:07 +0800

    test locks

--- src/app/api/lock/[seconds]/route.ts
+++ src/app/api/lock/[seconds]/route.ts
@@ -8,10 +8,13 @@ type Params = {
 export const GET = async (a: any,{params: {sleep}}: Params) => {
   try {
 	await admin(async conn => {
+	  console.log("locking");
 	  await conn.query("FLUSH TABLES WITH READ LOCK;");
 	  await new Promise(resolve => setTimeout(resolve, Number(sleep) * 1000));
+	  console.log("unlocking");
 	  await conn.query("UNLOCK TABLES;");
 	});
+	console.log("unlocked");
 	return NextResponse.json({ success: true });
   } catch (e) {
 	console.error(e);
