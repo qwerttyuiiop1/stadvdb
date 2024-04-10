7167eb0cf414aa43dcb9d3031eafd57f98191af5
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-04-10 13:07:10 +0800

    implement search params backend

--- src/app/api/appointments/route.ts
+++ src/app/api/appointments/route.ts
@@ -3,17 +3,63 @@ import { read, write } from "@connect";
 import { Appointment } from "@/components/Table/TableRow";
 
 
+
 export const GET = async (req: NextRequest) => {
   const params = req.nextUrl.searchParams;
   const limit = Number(params.get("limit"));
   const page = Number(params.get("page")) || 1;
+  const conditions = [] as string[];
+  const whereValues = [] as string[];
+  if (params.has("pxid")) {
+	conditions.push("pxid = ?");
+	whereValues.push(params.get("pxid")!);
+  }
+  if (params.has("clinicid")) {
+	conditions.push("clinicid = ?");
+	whereValues.push(params.get("clinicid")!);
+  }
+  if (params.has("doctorid")) {
+	conditions.push("doctorid = ?");
+	whereValues.push(params.get("doctorid")!);
+  }
+  if (params.has("status")) {
+	conditions.push("status = ?");
+	whereValues.push(params.get("status")!);
+  }
+  if (params.has("timequeued")) {
+	conditions.push("timequeued = ?");
+	whereValues.push(params.get("timequeued")!);
+  }
+  if (params.has("queuedate")) {
+	conditions.push("queuedate = ?");
+	whereValues.push(params.get("queuedate")!);
+  }
+  if (params.has("starttime") && params.has("endtime")) {
+	conditions.push("starttime BETWEEN ? AND ?");
+	whereValues.push(params.get("starttime")!);
+	whereValues.push(params.get("endtime")!);
+  }
+  if (params.has("type")) {
+	conditions.push("type = ?");
+	whereValues.push(params.get("type")!);
+  }
+  if (params.has("virtual")) {
+	conditions.push("virtual = ?");
+	whereValues.push(params.get("virtual")!);
+  }
+  if (params.has("apptid")) {
+	conditions.push("apptid = ?");
+	whereValues.push(params.get("apptid")!);
+  }
+  const where = conditions.length ? " WHERE " + conditions.join(" AND ") : "";
+  const order = " ORDER BY apptid DESC" +
+	(limit ? ` LIMIT ${limit} OFFSET ${(page - 1) * limit}` : "") + ";";
+  
   try {
-	const order = " ORDER BY apptid DESC" +
-		(limit ? ` LIMIT ${limit} OFFSET ${(page - 1) * limit}` : "");
     const res = await read(async (conn) => {
-      return conn.sql("SELECT * FROM appointments" + order)
+      return conn.query("SELECT * FROM appointments" + where + order, whereValues);
     }, "READ COMMITTED");
-    return NextResponse.json({ appointments: res });
+    return NextResponse.json({ appointments: res[0] });
   } catch (e) {
     console.error(e);
     return NextResponse.json(e, { status: 500 });
