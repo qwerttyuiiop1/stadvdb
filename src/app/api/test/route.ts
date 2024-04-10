909835b5736ae8421a10e10217ec54152a60e5e5
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-04-10 15:31:09 +0800

    update test to correct type

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,7 +3,7 @@ import { read, write } from "@connect";
 export const GET = async (req: NextRequest) => {
   try {
 	const res = await read(async conn => {
-		return conn.sql('SELECT status as bar, apptid as id FROM appointments WHERE id="1"')
+		return conn.sql('SELECT status, apptid FROM appointments WHERE apptid="1"')
 	}, "READ COMMITTED");
 	return NextResponse.json(res[0]);
   } catch (e) {
@@ -15,8 +15,8 @@ export const PUT = async (req: NextRequest) => {
   try {
 	const { bar } = await req.json();
 	const res = await write(async conn => {
-		await conn.query('UPDATE status SET statys = ? WHERE id = "1"', [bar])
-		return conn.sql('SELECT status as bar, apptid as id FROM appointments WHERE id="1"')
+		await conn.query('UPDATE status SET statys = ? WHERE apptid = "1"', [bar])
+		return conn.sql('SELECT status, apptid FROM appointments WHERE apptid="1"')
 	}, "REPEATABLE READ");
 	return NextResponse.json(res[0]);
   } catch (e) {
