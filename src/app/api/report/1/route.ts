5ac6d721c80262eaf1d61eb2bacc56fc02250cbf
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-04-11 23:31:51 +0800

    format sql

--- src/app/api/report/1/route.ts
+++ src/app/api/report/1/route.ts
@@ -6,15 +6,13 @@ Total patient count per health facility in Visayas
 `
 export const GET = async () => {
   const res = await read(async conn => 
-	conn.sql(`SELECT c . clinicid ,
-	c . regionname ,
-	COUNT ( DISTINCT a . pxid ) AS ' patient_count '
+	conn.sql(`SELECT c.clinicid, c.regionname, COUNT(DISTINCT a.pxid) AS 'patient_count'
 	FROM clinics c
 	LEFT JOIN appointments a
-	ON c . clinicid = a . clinicid
-	WHERE c . regionname LIKE '% Visayas % '
-	GROUP BY c . clinicid , c . regionname
-	ORDER BY c . clinicid ;`)
+	ON c.clinicid = a.clinicid
+	WHERE c.regionname LIKE '%Visayas%'
+	GROUP BY c.clinicid, c.regionname
+	ORDER BY c.clinicid ;`)
   )
   return NextResponse.json({
 	res, desc
